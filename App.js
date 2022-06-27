1c66495ff72e7a8e4f3bca37cbda8f8970faa89d
Liam Carswell <liamcarswell@gmail.com>
2022-06-27 12:54:01 +0100

    add google login functionality

--- App.js
+++ App.js
@@ -1,16 +1,35 @@
 import * as Sharing from "expo-sharing";
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Button, Text } from "react-native";
 import BookList from "./components/BookList";
 import Header from "./components/Header";
 
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC15hpnCra3iuHNw9q1gbxerBHY5MZalEA",
+  authDomain: "switchbook-225b3.firebaseapp.com",
+  projectId: "switchbook-225b3",
+  storageBucket: "switchbook-225b3.appspot.com",
+  messagingSenderId: "731886943527",
+  appId: "1:731886943527:web:af3ffaf1fd4932b30cfd02",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
 export default function App() {
+  const [user] = useAuthState(auth);
+
   return (
     <View style={styles.container}>
       <Header />
       <BookList />
       <StatusBar style="auto" />
+      {user ? <LogoutButton /> : <LoginButton />}
     </View>
   );
 }
@@ -23,3 +42,34 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
+const LoginButton = () => {
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider);
+  };
+
+  return (
+    <View>
+      <Button
+        onPress={signInWithGoogle}
+        title="Continue with Google"
+        color="#841584"
+        accessibilityLabel="Login/signup with your Google account"
+      />
+    </View>
+  );
+};
+
+const LogoutButton = () => {
+  return (
+    <View>
+      <Button
+        onPress={() => auth.signOut()}
+        title="Logout"
+        color="grey"
+        accessibilityLabel="Logout of your account"
+      />
+    </View>
+  );
+};
