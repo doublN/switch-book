709b0bb3b6e9e104854a5fd5ef44ee172bb457c7
Nathan Newberry <nathannewberry@172-10-254-188.lightspeed.fyvlar.sbcglobal.net>
2022-06-27 14:24:46 +0100

    Cleaning up

--- components/BookList.jsx
+++ components/BookList.jsx
@@ -2,8 +2,6 @@ import { Text, View, FlatList, ScrollView, Image } from "react-native";
 import React, { useState } from "react";
 
 const BookList = () => {
-
-
   const [books, setBooks] = useState([
     {
       title: "Surely you're joking, Mr Feynman",
@@ -15,10 +13,6 @@ const BookList = () => {
       imageURI : "https://images-na.ssl-images-amazon.com/images/I/71yPWidjDaL.jpg",
     },
   ]);
-
-  const image = (props) => (
-    <Image source={{uri : props.imageURI}}/>
-  )
 
   return (
     <View>
