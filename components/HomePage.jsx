730cbe8387fca6a98564e4d4f86d6b7cf364db99
Nathan Newberry <nathannewberry@172-10-254-188.lightspeed.fyvlar.sbcglobal.net>
2022-06-29 14:02:43 +0100

    Progress with firestore access and query

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -1,11 +1,43 @@
 import { Button, View, Text } from "react-native";
 import BookList from "./BookList"
 import { useContext } from "react";
-import FirebaseContext from ".FirebaseContext./Contexts/FirebaseContext";
+import FirebaseContext from "../Contexts/FirebaseContext";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 
 const HomePage = ({ navigation }) => {
 
-  const {user} = useContext(FirebaseContext);
+  const {user, firestore} = useContext(FirebaseContext);
+  const usersRef = collection(firestore, "users");
+  
+  // const users = useCollectionData(queryUser, { idField: "id" });
+  
+  // const UserDbQuery = async() =>{
+  //   const queryUser = query(usersRef, where("uid", "==", user.uid));
+  //   const querySnapshot = await getDocs(queryUser);
+  //   querySnapshot.forEach((doc) => {
+  //    // doc.data() is never undefined for query doc snapshots
+  //    console.log(doc.id, " => ", doc.data());
+  //   })
+
+  //   return querySnapshot;
+  // };
+
+  const queryUser = query(usersRef, where("uid", "==", user.uid));
+  
+  // getDocs(queryUser).then((querySnapshot) =>{
+  //   querySnapshot.forEach((doc) => {
+  //     // doc.data() is never undefined for query doc snapshots
+  //     console.log(doc.id, " => ", doc.data());
+  //    })
+  // })
+
+  firestore.collection('users').get().then((querySnapshot) => {
+    querySnapshot.forEach(snapshot => {
+        let data = snapshot.data();
+        console.log(data);
+    })
+  })
 
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
