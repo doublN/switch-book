f99c37a0c2345cf9d42e0f6d1511835e35760bf2
Liam Carswell <liamcarswell@gmail.com>
2022-06-30 17:01:10 +0100

    add default successful swap and rating to createUser

--- Utils/dbQueries.js
+++ Utils/dbQueries.js
@@ -1,11 +1,11 @@
 import {
-    collection,
-    query,
-    where,
-    getDocs,
-    setDoc,
-    doc,
-    orderBy,
+  collection,
+  query,
+  where,
+  getDocs,
+  setDoc,
+  doc,
+  orderBy,
 } from "firebase/firestore";
 import { firestore } from "./firebase";
 
@@ -13,45 +13,45 @@ export const getUserByUid = async (uid) => {
   const usersRef = collection(firestore, "users");
   const queryUser = query(usersRef, where("uid", "==", uid));
 
-
-    try {
-        const querySnapshot = await getDocs(queryUser);
-        let users = [];
-        querySnapshot.forEach((docs) => users.push(docs.data()));
-        const currentUser = users[0];
-        return currentUser;
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    const querySnapshot = await getDocs(queryUser);
+    let users = [];
+    querySnapshot.forEach((docs) => users.push(docs.data()));
+    const currentUser = users[0];
+    return currentUser;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const createUser = async (
-    username,
-    location,
-    authorisedUser,
-    selectedImage
+  username,
+  location,
+  authorisedUser,
+  selectedImage
 ) => {
-    try {
-        await setDoc(doc(firestore, "users", authorisedUser), {
-            // insert data for database here (needs to include 'uid: authorisedUser.uid')
-            uid: authorisedUser,
-            username,
-            location,
-            selectedImage,
-        });
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    await setDoc(doc(firestore, "users", authorisedUser), {
+      uid: authorisedUser,
+      username,
+      location,
+      selectedImage,
+      successfulSwaps: 0,
+      rating: 0,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const getBooks = async (searchText) => {
   const booksRef = collection(firestore, "books");
   const queryBooks = query(booksRef, orderBy("dateAdded", "desc"));
 
-    try {
-        const querySnapshot = await getDocs(queryBooks);
-        let books = [];
-        querySnapshot.forEach((docs) => books.push(docs.data()));
+  try {
+    const querySnapshot = await getDocs(queryBooks);
+    let books = [];
+    querySnapshot.forEach((docs) => books.push(docs.data()));
 
     if (searchText) {
       const filteredBooks = books.filter((book) => {
@@ -64,10 +64,10 @@ export const getBooks = async (searchText) => {
       return filteredBooks;
     }
 
-        return books;
-    } catch (err) {
-        console.log(err);
-    }
+    return books;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const getSwapsByIsbn = async (isbn) => {
