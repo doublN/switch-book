5bf015c41a4e6b8ec23105e0272ea0d9b3a686c8
Malavika Mysore <malavika.mysore@gmail.com>
2022-07-01 14:34:12 +0100

    displays books from google api

--- components/AddABookScreen.jsx
+++ components/AddABookScreen.jsx
@@ -11,23 +11,7 @@ import {
 import React, { useEffect, useState } from "react";
 
 export default function AddABookScreen() {
-  const [books, setBooks] = useState([
-    {
-      id: "3gDP8kq561wC",
-      volumeInfo: {
-        title: "Jane Austen's Letters",
-        authors: ["Jane Austen", "Deirdre Le Faye"],
-
-        description:
-          "The fourth edition of Jane Austen's Letters incorporates the findings of new scholarship to enrich our understanding of Austen and give us the fullest view yet of her life and family. The biographical and topographical indexes have been updated, a new subject index has been created, and the contents of the notes added to the general index.",
-        categories: ["Biography & Autobiography"],
-        imageLinks: {
-          thumbnail:
-            "http://books.google.com/books/content?id=3gDP8kq561wC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
-        },
-      },
-    },
-  ]);
+  const [books, setBooks] = useState([]);
   const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
@@ -47,8 +31,9 @@ export default function AddABookScreen() {
           return (
             <>
               <Image
+                style={{ resizeMode: "contain", height: 200, width: 100 }}
                 source={{
-                  uri: "http://books.google.com/books/content?id=3gDP8kq561wC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+                  uri: `${book.volumeInfo.imageLinks.thumbnail}`,
                 }}
               />
               <Text> {book.volumeInfo.title}</Text>
